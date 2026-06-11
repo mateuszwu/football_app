75c8392d7b0075f0fb2a50c8b1ad0ae5926c87e0
Mateusz Wieczorek <mateusz.wu@gmail.com>
2026-06-11 21:35:28 +0200

    feat(ai-51): finish match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,6 @@
 class MatchesController < ApplicationController
+  before_action :require_admin!, only: :finish
+
   def show
     match = Match
       .includes(
@@ -9,5 +11,15 @@ class MatchesController < ApplicationController
       .find(params[:id])
 
     render :show, locals: { match: match }
+  end
+
+  def finish
+    match = Match.find(params[:id])
+
+    if Matches::FinishMatch.call(match:)
+      redirect_to match_path(match), notice: "Match finished"
+    else
+      redirect_to match_path(match), alert: "Could not finish match"
+    end
   end
 end
