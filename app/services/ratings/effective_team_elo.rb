a17b1463b3df7891bd2044e354f3b7497cb098f7
Mateusz Wieczorek <mateusz.wu@gmail.com>
2026-06-12 19:49:26 +0200

    feat(ai): calculate effective team elo

--- app/services/ratings/effective_team_elo.rb
+++ app/services/ratings/effective_team_elo.rb
@@ -1,33 +1,12 @@
 module Ratings
   class EffectiveTeamElo
-    PLAYER_COUNT_ADVANTAGE = 40
-
-    def self.call(players:, opponent_players:, elo_map:)
-      new(players:, opponent_players:, elo_map:).call
-    end
-
-    def initialize(players:, opponent_players:, elo_map:)
-      @players = players
-      @opponent_players = opponent_players
-      @elo_map = elo_map
-    end
-
-    def call
-      return 0.0 if players.empty?
-
-      average_elo + player_count_advantage
-    end
-
-    private
-
-    attr_reader :players, :opponent_players, :elo_map
-
-    def average_elo
-      players.sum { |player| elo_map[player.id] }.to_f / players.size
-    end
-
-    def player_count_advantage
-      (players.size - opponent_players.size) * PLAYER_COUNT_ADVANTAGE
+    def self.call(players:, opponent_players:, elo_map:, season:)
+      CalculateTeamElo.call(
+        players: players,
+        opponent_players: opponent_players,
+        elo_map: elo_map,
+        season: season
+      ).effective_elo
     end
   end
 end
